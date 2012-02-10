0118b62fb96670fc55f0bb1a4a4c649308bee7a2
Volodymyr <vdubyna@speroteck.com>
2012-02-10 11:53:00 +0200

    autoload

--- autoload.php
+++ autoload.php
@@ -1,16 +1,5 @@
 <?php
-require_once('ClassLoader/UniversalClassLoader.php');
 
-$loader = new \Symfony\Component\ClassLoader\UniversalClassLoader();
-$loader->registerNamespaces(array(
-    'Testns'          => __DIR__,
-));
-
-$loader->registerPrefixes(array(
-    'Testpr_' => __DIR__,
-));
-
-$loader->register();
-use Testns\Auto;
-$testns = new Auto();
-$testpr = new Testpr_Auto();
+function __autoload($className) {
+   require_once $className . '.php';
+}
